fbb49ec8d02dd8d1e3ddabcb070c211af5c6a599
jacan <jckandersen@gmail.com>
2026-05-15 13:47:18 +0200

    Time series and working with pandas

--- telemetry-analyzer/src/telemetry_analyzer/analyzer.py
+++ telemetry-analyzer/src/telemetry_analyzer/analyzer.py
@@ -12,3 +12,24 @@ def count_errors_by_service(df: pd.DataFrame) -> pd.Series:
     errors = df[df["level"] == "ERROR"]
 
     return errors.groupby("service").size()
+
+def sort_by_timestamp(df: pd.DataFrame) -> pd.DataFrame:
+    return df.sort_values(by="timestamp", ascending=False)
+
+def events_per_service(df: pd.DataFrame) -> pd.Series:
+    return df.groupby("service").size()
+
+def events_per_minute(df: pd.DataFrame) -> pd.Series:
+    return df.set_index("timestamp").resample("1min").size()
+
+def rolling_error_count(df: pd.DataFrame) -> pd.Series:
+    errors = df[df["level"] == "ERROR"]
+
+    return (
+        errors
+        .set_index("timestamp")
+        .resample("1min")
+        .size()
+        .rolling(window=3)
+        .mean()
+    )
--- telemetry-analyzer/src/telemetry_analyzer/main.py
+++ telemetry-analyzer/src/telemetry_analyzer/main.py
@@ -1,11 +1,18 @@
 from rich import print
 from pathlib import Path
 
+from telemetry_analyzer.printer import (
+    print_series_for_debug,
+    print_series_for_debug
+)
+
 from telemetry_analyzer.parser import load_logs
 from telemetry_analyzer.analyzer import (
     logs_to_data_frame,
     get_errors,
-    count_errors_by_service
+    count_errors_by_service,
+    events_per_minute,
+    rolling_error_count
 )
 
 def main():
@@ -20,5 +27,15 @@ def main():
 
     print(error_count)
     print(errors)
+    print(dataframe.dtypes)
+
+    # Time series..
+    print("Time series:")
+    events = events_per_minute(dataframe)
+
+    print("Rolling Windows")
+    error_window = rolling_error_count(dataframe)
+    print_series_for_debug(error_window)
+
 
 if __name__ == "__main__": main()
